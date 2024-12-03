f0e26a1cd8d97d83fca37181cf8f54b85cf836e2
dan7467 <dan7467@gmail.com>
2024-12-04 01:44:48 +0200

    added dark theme

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('my_profile/', views.my_profile, name='my_profile')
+    path('my_profile/', views.my_profile, name='my_profile'),
+    path("toggle-dark-theme/", views.toggle_dark_theme, name="toggle_dark_theme"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,14 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.http import JsonResponse
+
+@login_required
+def toggle_dark_theme(request):
+    if request.method == "POST":
+        request.session["dark_theme"] = request.POST.get("dark_theme")
+        return JsonResponse({"status": "success", "dark_theme": request.session["dark_theme"]})
+    return JsonResponse({"status": "error", "message": "Invalid request method"}, status=400)
 
 def home(request):
     return render(request, 'home.html')
